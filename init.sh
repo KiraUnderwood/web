2c44a84c77d11b3db15dd8442d0be9c268f6f0d2
KiraUnderwood <46092865+KiraUnderwood@users.noreply.github.com>
2019-01-05 17:45:09 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,10 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd /home/box/web
-sudo gunicorn -b 0.0.0.0:8080 hello:application
+sudo gunicorn -c /home/box/web/etc/wsgi.example hello:application
+cd /home/box/web/ask
+sudo gunicorn -c /home/box/web/etc/django.conf ask.wsgi:application
+##sudo gunicorn -b 0.0.0.0:8080 hello:application
 ##sudo ln -sf /home/box/web/etc/wsgi.example /etc/gunicorn.d/test
 ##sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 ##sudo /etc/init.d/gunicorn restart test 
