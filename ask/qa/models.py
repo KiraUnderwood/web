b64a7dec7218efd6df4ba21678fcb51609a5f198
KiraUnderwood <46092865+KiraUnderwood@users.noreply.github.com>
2019-01-08 00:42:19 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,36 +1,34 @@
 from __future__ import unicode_literals
 
-from django.db import models
-
 # Create your models here.
 
 
-class Question(models.Model):
-       title = models.CharField(max_length=255)
-       text = models.TextField()
-       added_at = models.DateTimeField(blank=True)
-       rating = 
-       author = 
-       likes = 
-def __unicode__(self):
-return self.title
-def get_absolute_url(self):
-return '/post/%d/' % self.pk
-class Meta:
-db_table = 'blogposts'
-ordering = ['-creation_date']
+from django.db import models
+from django.contrib.auth.models import User
 
+
+# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(default="", max_length=1024)
+    text = models.TextField(default="")
+    added_at = models.DateField(null=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name="q")
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return "/question/{}/".format(self.id)
 
 
 class Answer(models.Model):
-       text = models.TextField()
-       added_at = models.DateTimeField(blank=True)
-       question = 
-       author = 
-def __unicode__(self):
-return self.title
-def get_absolute_url(self):
-return '/post/%d/' % self.pk
-class Meta:
-db_table = 'blogposts'
-ordering = ['-creation_date']
+    text = models.TextField(default="")
+    added_at = models.DateField(null=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.text
