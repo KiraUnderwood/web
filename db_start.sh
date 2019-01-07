f2852ba125f3244d58a5af8bbae4718887851e8c
KiraUnderwood <46092865+KiraUnderwood@users.noreply.github.com>
2019-01-07 21:39:03 +0300

    Update db_start.sh

--- db_start.sh
+++ db_start.sh
@@ -1,3 +1,9 @@
 sudo /etc/init.d/mysql start
 
 mysql -u root -e "CREATE DATABASE kira";
+
+mysql -u root -e "CREATE USER 'kira'@'%' IDENTIFIED BY 'password'"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'kira'@'%' WITH GRANT OPTION"
+
+exit
+mysql -u kira --password=password kira
