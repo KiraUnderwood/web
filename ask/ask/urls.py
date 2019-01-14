f57fb8f6727add418ce3cb2d2d6fc4efaf359141
KiraUnderwood <46092865+KiraUnderwood@users.noreply.github.com>
2019-01-14 17:44:14 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     url(r'^$', recent),
     url(r'^login/', include('qa.urls')),
     url(r'^signup/', include('qa.urls')),
-    url(r'^question/', include('qa.urls')),
+    url(r'^question/(?P<num>\d+)/', question),
+    ##url(r'^question/', include('qa.urls')),
     url(r'^ask/', include('qa.urls')),
     url(r'^popular/', popular),
 
