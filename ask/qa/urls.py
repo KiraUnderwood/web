464317c460e3822026ead737647773d6256bcc56
KiraUnderwood <46092865+KiraUnderwood@users.noreply.github.com>
2019-01-14 14:48:39 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from . import views
 from qa.views import question
 
 
 urlpatterns = [
-  url(r'^$(?P<num>\d+/$)', question),
+  url(r'^$(?P<num>)\d+/$)', question),
 ]
