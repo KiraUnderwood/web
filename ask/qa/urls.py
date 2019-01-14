43737a8435ab52169576e5522ebc31371ca2b033
KiraUnderwood <46092865+KiraUnderwood@users.noreply.github.com>
2019-01-14 17:41:49 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import url
-from qa.views import question
-
+##from qa.views import question
+'''
 
 urlpatterns = [
   url(r'^$(?P<num>)\d+/$', question),
 ]
+'''
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^', include('qa.urls')),
+    #url(r'^admin/', include(admin.site.urls)),
+)
