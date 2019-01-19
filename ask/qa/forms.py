5a3c9199ea4994d8ebc283215d69deacf4c5a300
KiraUnderwood <46092865+KiraUnderwood@users.noreply.github.com>
2019-01-19 23:06:12 +0300

    Update forms.py

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 
-from qa.models import Question,Answer
+from qa.models import Question, Answer
+
 
 class AskForm(forms.Form):
     title = forms.CharField(max_length=1024)
     text = forms.CharField()
+
+    def clean(self):
+        pass
 
     def save(self):
         q = Question(**self.cleaned_data)
@@ -12,13 +16,22 @@ class AskForm(forms.Form):
         return q
 
 
-
 class AnswerForm(form.Forms):
     text = forms.CharField()
     question = forms.IntegerField(widget=forms.HiddenInput)
 
-    def clean(self):
-        
+    def clean_question(self):
+        question_id = self.cleaned_data['question']
+        try:
+            question = Question.objects.get(id=question_id)
+        except Question.DoesNotExist:
+            question = None
+        return question
 
+    def clean(self):
+        pass
 
     def save(self):
+        a = Answer(**self.cleaned_data)
+        a.save()
+        return a
